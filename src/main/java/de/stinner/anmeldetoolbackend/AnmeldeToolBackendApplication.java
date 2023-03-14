19c727593b77f332d5b3ef5c5d7fc8aab988b200
Laurenz Stinner <laurenz.stinner2@gmail.com>
2023-03-15 00:21:11 +0100

    Init Backend

---
+++ src/main/java/de/stinner/anmeldetoolbackend/AnmeldeToolBackendApplication.java
@@ -0,0 +1,13 @@
+package de.stinner.anmeldetoolbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnmeldeToolBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnmeldeToolBackendApplication.class, args);
+	}
+
+}
