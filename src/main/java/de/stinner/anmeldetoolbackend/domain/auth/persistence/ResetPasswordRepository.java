d2a2c356944d1a544e697c3d468c1a0bbe2a0684
Laurenz Stinner <laurenz.stinner2@gmail.com>
2023-03-24 15:25:50 +0100

    Added entity and db-script for reset-password

---
+++ src/main/java/de/stinner/anmeldetoolbackend/domain/auth/persistence/ResetPasswordRepository.java
@@ -0,0 +1,17 @@
+package de.stinner.anmeldetoolbackend.domain.auth.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.Instant;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ResetPasswordRepository extends JpaRepository<ResetPasswordEntity, UUID> {
+
+
+//    Optional<ResetPasswordEntity> findByResetIdAndCreatedAtIsAfterAndEmailSentIsTrue(@Param("resetId") UUID resetId, @Param("createdAt") Instant after);
+
+    Optional<ResetPasswordEntity> findByResetIdAndEmailSentAfter(UUID resetId, Instant after);
+
+    void deleteAllByUserEquals(UserDataEntity entity);
+}
