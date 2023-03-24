249642e4b67a148933e9b44c0610a07a6a24b3a0
Laurenz Stinner <laurenz.stinner2@gmail.com>
2023-03-24 15:26:55 +0100

    Added DTOs and Endpoints for forgot password and reset

--- src/main/java/de/stinner/anmeldetoolbackend/domain/auth/api/AuthController.java
+++ src/main/java/de/stinner/anmeldetoolbackend/domain/auth/api/AuthController.java
@@ -2,14 +2,16 @@ package de.stinner.anmeldetoolbackend.domain.auth.api;
 
 import de.stinner.anmeldetoolbackend.application.rest.ApiEndpoints;
 import de.stinner.anmeldetoolbackend.domain.auth.api.model.FinishRegistrationDto;
+import de.stinner.anmeldetoolbackend.domain.auth.api.model.ForgotPasswordRequestDto;
 import de.stinner.anmeldetoolbackend.domain.auth.api.model.RegistrationRequestDto;
+import de.stinner.anmeldetoolbackend.domain.auth.api.model.ResetPasswordDto;
 import de.stinner.anmeldetoolbackend.domain.auth.persistence.RegistrationEntity;
 import de.stinner.anmeldetoolbackend.domain.auth.persistence.UserDataEntity;
 import de.stinner.anmeldetoolbackend.domain.auth.service.AuthenticationService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,7 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@Validated
 public class AuthController {
     private final AuthenticationService authenticationService;
 
@@ -33,18 +34,30 @@ public class AuthController {
 
 
     @PostMapping(ApiEndpoints.V1.Auth.REGISTER)
-    public ResponseEntity<Void> register(@RequestBody RegistrationRequestDto registerRequestDto) {
+    public ResponseEntity<Void> register(@Valid @RequestBody RegistrationRequestDto registerRequestDto) {
         authenticationService.register(RegistrationEntity.of(registerRequestDto));
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @PostMapping(ApiEndpoints.V1.Auth.FINISH_REGISTRATION)
-    public ResponseEntity<Void> finishRegistration(@RequestBody FinishRegistrationDto finishRegistrationDto) {
+    public ResponseEntity<Void> finishRegistration(@Valid @RequestBody FinishRegistrationDto finishRegistrationDto) {
         UserDataEntity entity = authenticationService.finishRegistration(
-                finishRegistrationDto.getId(),
+                finishRegistrationDto.getRegistrationId(),
                 finishRegistrationDto.getPassword()
         );
 
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PostMapping(ApiEndpoints.V1.Auth.FORGOT_PASSWORD)
+    public ResponseEntity<Void> forgotPassword(@Valid @RequestBody ForgotPasswordRequestDto forgotPasswordRequestDto) {
+        authenticationService.forgotPassword(forgotPasswordRequestDto.getEmail());
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PostMapping(ApiEndpoints.V1.Auth.RESET_PASSWORD)
+    public ResponseEntity<Void> resetPassword(@Valid @RequestBody ResetPasswordDto resetPasswordRequestDto) {
+        authenticationService.resetPassword(resetPasswordRequestDto.getResetId(), resetPasswordRequestDto.getPassword());
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
