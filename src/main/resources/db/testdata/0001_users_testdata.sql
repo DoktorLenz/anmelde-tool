d789b6fdd9207519f8c6e702e638a82eeb4d8000
Laurenz Stinner <laurenz.stinner2@gmail.com>
2023-03-25 02:53:15 +0100

    use correct password in testdata

--- src/main/resources/db/testdata/0001_users_testdata.sql
+++ src/main/resources/db/testdata/0001_users_testdata.sql
@@ -8,5 +8,5 @@ from users_;
 insert into users_ (id, email, password, account_locked, credentials_expired, enabled, authorities, created_at,
                     firstname, lastname)
 values ('8a01516e-a70a-4613-9972-6168c1704795', 'laurenz.stinner2@gmail.com',
-        '$2a$10$SALfL1nHCsctCL/RGvMdze76XbVBskDyXbWIGxMYSwD5enA3ToaUi', false, false, true,
+        '$2a$10$xXztkzVsBXuzQ66aF6flxeJrsp.RwzDZKwf5ldngAJAHKRbT02qsi', false, false, true,
         ARRAY ['ROLE_USER'::authority, 'ROLE_ADMIN'::authority], '2021-09-10 21:10:00.000000+00', 'admin', 'admin');
