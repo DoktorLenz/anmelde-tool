82a657d17b3bcca5eba59c5a7e17d0b1d1dfaf4a
Laurenz Stinner <laurenz.stinner2@gmail.com>
2023-03-25 01:55:05 +0100

    changed mail adress in testdata

--- src/main/resources/db/testdata/0001_users_testdata.sql
+++ src/main/resources/db/testdata/0001_users_testdata.sql
@@ -7,6 +7,6 @@ from users_;
 
 insert into users_ (id, email, password, account_locked, credentials_expired, enabled, authorities, created_at,
                     firstname, lastname)
-values ('8a01516e-a70a-4613-9972-6168c1704795', 'admin@dpsgkolbermoor.de',
+values ('8a01516e-a70a-4613-9972-6168c1704795', 'laurenz.stinner2@gmail.com',
         '$2a$10$SALfL1nHCsctCL/RGvMdze76XbVBskDyXbWIGxMYSwD5enA3ToaUi', false, false, true,
         ARRAY ['ROLE_USER'::authority, 'ROLE_ADMIN'::authority], '2021-09-10 21:10:00.000000+00', 'admin', 'admin');
