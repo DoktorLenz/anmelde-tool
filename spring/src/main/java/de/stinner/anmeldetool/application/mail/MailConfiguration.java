0b15bd8ca31e97f2b3269b874b5c6acc01c404ec
Laurenz Stinner <laurenz.stinner2@gmail.com>
2023-04-02 04:15:57 +0200

    Fixed Repository issue

--- spring/src/main/java/de/stinner/anmeldetool/application/mail/MailConfiguration.java
+++ spring/src/main/java/de/stinner/anmeldetool/application/mail/MailConfiguration.java
@@ -1,10 +1,7 @@
 package de.stinner.anmeldetool.application.mail;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.*;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
