ecca4db7671b1ca11f33c61470b3d34d7ca190a6
Laurenz Stinner <laurenz.stinner2@gmail.com>
2023-04-03 03:47:47 +0200

    set test user

--- spring/src/main/resources/db/testdata/0001_users_testdata.sql
+++ spring/src/main/resources/db/testdata/0001_users_testdata.sql
@@ -7,6 +7,6 @@ from users_;
 
 insert into users_ (id, email, password, account_locked, credentials_expired, enabled, authorities, created_at,
                     firstname, lastname)
-values ('8a01516e-a70a-4613-9972-6168c1704795', 'laurenz.stinner2@gmail.com',
-        '$2a$10$xXztkzVsBXuzQ66aF6flxeJrsp.RwzDZKwf5ldngAJAHKRbT02qsi', false, false, true,
+values ('8a01516e-a70a-4613-9972-6168c1704795', 'bar@localhost',
+        '$2a$10$WQwYGmfQ1Pw1B9/Cexkuoep97NWjuyJebJlHaFqnIBtWP7hDwrYH2', false, false, true,
         ARRAY ['ROLE_USER'::authority, 'ROLE_ADMIN'::authority], '2021-09-10 21:10:00.000000+00', 'admin', 'admin');
