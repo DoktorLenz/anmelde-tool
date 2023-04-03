3d0292faca5f44e681bebfba23d760b069b7e39a
Laurenz Stinner <laurenz.stinner2@gmail.com>
2023-04-03 22:55:42 +0200

    Chained and simplified assertions

--- spring/src/test/java/de/stinner/anmeldetool/domain/auth/api/model/FinishRegistrationDtoTest.java
+++ spring/src/test/java/de/stinner/anmeldetool/domain/auth/api/model/FinishRegistrationDtoTest.java
@@ -1,9 +1,4 @@
 package de.stinner.anmeldetool.domain.auth.api.model;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
-import java.util.Set;
-import java.util.UUID;
 
 import de.stinner.anmeldetool.application.constraints.Password;
 import jakarta.validation.ConstraintViolation;
@@ -14,7 +9,13 @@ import jakarta.validation.constraints.NotNull;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-public class FinishRegistrationDtoTest {
+
+import java.util.Set;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FinishRegistrationDtoTest {
     @Test
     @DisplayName("FinishRegistrationDto should have working getters and setters")
     void testGettersAndSetters() {
@@ -51,30 +52,30 @@ public class FinishRegistrationDtoTest {
         Validator validator = factory.getValidator();
 
         Set<ConstraintViolation<FinishRegistrationDto>> violations = validator.validate(dto);
-        assertThat(violations).hasSize(2);
-
-        assertThat(violations).anySatisfy(violation -> {
-            assertThat(violation.getRootBean()).isEqualTo(dto);
-            assertThat(violation.getPropertyPath().toString()).isEqualTo("registrationId");
-            assertThat(violation.getConstraintDescriptor().getAnnotation()).isInstanceOf(NotNull.class);
-        });
-
-        assertThat(violations).anySatisfy(violation -> {
-            assertThat(violation.getRootBean()).isEqualTo(dto);
-            assertThat(violation.getPropertyPath().toString()).isEqualTo("password");
-            assertThat(violation.getConstraintDescriptor().getAnnotation()).isInstanceOf(Password.class);
-        });
+        assertThat(violations)
+                .hasSize(2)
+                .anySatisfy(violation -> {
+                    assertThat(violation.getRootBean()).isEqualTo(dto);
+                    assertThat(violation.getPropertyPath()).hasToString("registrationId");
+                    assertThat(violation.getConstraintDescriptor().getAnnotation()).isInstanceOf(NotNull.class);
+                })
+                .anySatisfy(violation -> {
+                    assertThat(violation.getRootBean()).isEqualTo(dto);
+                    assertThat(violation.getPropertyPath()).hasToString("password");
+                    assertThat(violation.getConstraintDescriptor().getAnnotation()).isInstanceOf(Password.class);
+                });
 
         dto.setRegistrationId(UUID.randomUUID());
         dto.setPassword("thispasswordistoolongtobevalidbecauseitexceedsfifty-sixcharacters");
 
         violations = validator.validate(dto);
-        assertThat(violations).hasSize(1);
-        assertThat(violations).allSatisfy(violation -> {
-            assertThat(violation.getRootBean()).isEqualTo(dto);
-            assertThat(violation.getPropertyPath().toString()).isEqualTo("password");
-            assertThat(violation.getConstraintDescriptor().getAnnotation()).isInstanceOf(Password.class);
-        });
+        assertThat(violations)
+                .hasSize(1)
+                .allSatisfy(violation -> {
+                    assertThat(violation.getRootBean()).isEqualTo(dto);
+                    assertThat(violation.getPropertyPath()).hasToString("password");
+                    assertThat(violation.getConstraintDescriptor().getAnnotation()).isInstanceOf(Password.class);
+                });
 
         dto.setPassword("validpassword");
 
