4a939597f21cf526296b31409ab91c750b9b6e6c
Laurenz Stinner <laurenz.stinner2@gmail.com>
2023-04-03 22:55:42 +0200

    Chained and simplified assertions

--- spring/src/test/java/de/stinner/anmeldetool/domain/auth/api/model/ChangePasswordDtoTest.java
+++ spring/src/test/java/de/stinner/anmeldetool/domain/auth/api/model/ChangePasswordDtoTest.java
@@ -1,15 +1,18 @@
 package de.stinner.anmeldetool.domain.auth.api.model;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import de.stinner.anmeldetool.application.constraints.Password;
-import jakarta.validation.*;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Set;
 
-public class ChangePasswordDtoTest {
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ChangePasswordDtoTest {
 
     @Test
     @DisplayName("ChangePasswordDto should have working getters and setters")
@@ -45,19 +48,18 @@ public class ChangePasswordDtoTest {
         Validator validator = factory.getValidator();
 
         Set<ConstraintViolation<ChangePasswordDto>> violations = validator.validate(dto);
-        assertThat(violations).hasSize(2);
-
-        assertThat(violations).anySatisfy(violation -> {
-            assertThat(violation.getRootBean()).isEqualTo(dto);
-            assertThat(violation.getPropertyPath().toString()).isEqualTo("oldPassword");
-            assertThat(violation.getConstraintDescriptor().getAnnotation()).isInstanceOf(Password.class);
-        });
-
-        assertThat(violations).anySatisfy(violation -> {
-            assertThat(violation.getRootBean()).isEqualTo(dto);
-            assertThat(violation.getPropertyPath().toString()).isEqualTo("newPassword");
-            assertThat(violation.getConstraintDescriptor().getAnnotation()).isInstanceOf(Password.class);
-        });
+        assertThat(violations)
+                .hasSize(2)
+                .anySatisfy(violation -> {
+                    assertThat(violation.getRootBean()).isEqualTo(dto);
+                    assertThat(violation.getPropertyPath()).hasToString("oldPassword");
+                    assertThat(violation.getConstraintDescriptor().getAnnotation()).isInstanceOf(Password.class);
+                })
+                .anySatisfy(violation -> {
+                    assertThat(violation.getRootBean()).isEqualTo(dto);
+                    assertThat(violation.getPropertyPath()).hasToString("newPassword");
+                    assertThat(violation.getConstraintDescriptor().getAnnotation()).isInstanceOf(Password.class);
+                });
 
         dto.setOldPassword("longenoughpassword");
         dto.setNewPassword("validpw123");
