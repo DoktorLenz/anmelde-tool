72f95a0f25ee6c8e15fb6f4b653ec4a8fb9a421b
Laurenz Stinner <laurenz.stinner2@gmail.com>
2023-04-03 21:10:05 +0200

    use restassured for API Tests

--- spring/src/test/java/de/stinner/anmeldetool/domain/auth/api/AuthControllerTest.java
+++ spring/src/test/java/de/stinner/anmeldetool/domain/auth/api/AuthControllerTest.java
@@ -2,24 +2,25 @@ package de.stinner.anmeldetool.domain.auth.api;
 
 import de.stinner.anmeldetool.application.rest.ApiEndpoints;
 import de.stinner.anmeldetool.base.BaseControllerTest;
-import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.test.context.support.WithMockUser;
+
 
 public class AuthControllerTest extends BaseControllerTest {
 
     @Test
-    @SneakyThrows
     public void testLogin() {
-        performGetRequestWithAuth(ApiEndpoints.V1.Auth.LOGIN, "bar@localhost", "validpassword")
-                .andExpect(status().isNoContent());
+        baseRequest.with()
+                .auth().basic("bar@localhost", "validpassword")
+                .when().get(ApiEndpoints.V1.Auth.LOGIN)
+                .then().statusCode(HttpStatus.NO_CONTENT.value());
     }
 
     @Test
     @WithMockUser
-    @SneakyThrows
     public void testLogout() {
-        performGetRequest(ApiEndpoints.V1.Auth.LOGOUT).andExpect(status().isNoContent());
-        performGetRequest(ApiEndpoints.V1.Auth.LOGIN).andExpect(status().is(401));
+        baseRequest.when().get(ApiEndpoints.V1.Auth.LOGOUT).then().statusCode(HttpStatus.NO_CONTENT.value());
+        baseRequest.when().get(ApiEndpoints.V1.Auth.LOGIN).then().statusCode(HttpStatus.UNAUTHORIZED.value());
     }
 }
