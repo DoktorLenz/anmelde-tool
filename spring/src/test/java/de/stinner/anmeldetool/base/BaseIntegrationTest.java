89f422f3e6829daa8535b5376066da38f2ccefb6
Laurenz Stinner <laurenz.stinner2@gmail.com>
2023-04-03 03:36:39 +0200

    Config Spring.Mail with code

--- spring/src/test/java/de/stinner/anmeldetool/base/BaseIntegrationTest.java
+++ spring/src/test/java/de/stinner/anmeldetool/base/BaseIntegrationTest.java
@@ -3,28 +3,56 @@ package de.stinner.anmeldetool.base;
 import com.icegreen.greenmail.configuration.GreenMailConfiguration;
 import com.icegreen.greenmail.junit5.GreenMailExtension;
 import com.icegreen.greenmail.util.ServerSetupTest;
+import jakarta.validation.constraints.NotNull;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.extension.RegisterExtension;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 
 
 @ActiveProfiles("integration-test")
 @SpringBootTest()
+@Slf4j
+@ContextConfiguration(initializers = BaseIntegrationTest.TestEnvInitializer.class)
 public abstract class BaseIntegrationTest {
 
-    @Value("${spring.mail.host}")
-    static private String mailServerHost;
+    public static final String mailServerHost = "localhost";
 
-    @Value("${spring.mail.username}")
-    static private String mailServerUsername;
+    public static final String mailServerUsername = "foo";
 
-    @Value("${spring.mail.password}")
-    static private String mailServerPassword;
+    public static final String mailServerPassword = "foo-pwd";
 
     @RegisterExtension
-    static GreenMailExtension greenMail = new GreenMailExtension(ServerSetupTest.SMTP_IMAP)
+    public static GreenMailExtension greenMail = new GreenMailExtension(ServerSetupTest.SMTP_IMAP)
             .withConfiguration(GreenMailConfiguration
                     .aConfig()
-                    .withUser(mailServerUsername + "@" + mailServerHost, mailServerUsername, mailServerPassword));
+                    .withUser(
+                            mailServerUsername + "@" + mailServerHost,
+                            mailServerUsername,
+                            mailServerPassword
+                    )
+            );
+
+    /**
+     * Overrides the specified properties.
+     */
+    static class TestEnvInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+        @Override
+        public void initialize(@NotNull ConfigurableApplicationContext applicationContext) {
+            final TestPropertyValues values = TestPropertyValues.of(
+                    "spring.mail.host=" + mailServerHost,
+                    "spring.mail.port=3025",
+                    "spring.mail.user=" + mailServerUsername,
+                    "spring.mail.password=" + mailServerPassword
+            );
+
+            values.applyTo(applicationContext);
+        }
+
+    }
 }
