04f5fb383a5acba4edcf61dca8512c80654a5cf3
Laurenz Stinner <laurenz.stinner2@gmail.com>
2023-04-05 18:13:42 +0200

    fix csrf issue in tests

--- spring/src/test/java/de/stinner/anmeldetool/base/BaseControllerTest.java
+++ spring/src/test/java/de/stinner/anmeldetool/base/BaseControllerTest.java
@@ -1,24 +1,30 @@
 package de.stinner.anmeldetool.base;
 
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import io.restassured.specification.RequestSpecification;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 
 
+@AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class BaseControllerTest extends BaseIntegrationTest {
-
-    protected static RequestSpecification baseRequest;
-
     @LocalServerPort
     int port;
+    @Autowired
+    private WebApplicationContext context;
 
     @BeforeEach
     public void setupRestAssured() {
         RestAssured.port = port;
-        baseRequest = RestAssured.given().contentType(ContentType.JSON);
+
+        RestAssuredMockMvc.webAppContextSetup(context);
+        RestAssuredMockMvc.postProcessors(csrf().asHeader());
     }
 }
