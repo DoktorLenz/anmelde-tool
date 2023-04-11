a88d4a9cae5595a744024c1d4045d8c795f22100
Laurenz Stinner <laurenz.stinner2@gmail.com>
2023-04-12 01:37:00 +0200

    page to inform user that registration was submitted

---
+++ angular/src/app/pages/auth/registration-sent/registration-sent.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RegistrationSentComponent } from './registration-sent.component';
+
+describe('RegistrationSentComponent', () => {
+  let component: RegistrationSentComponent;
+  let fixture: ComponentFixture<RegistrationSentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RegistrationSentComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(RegistrationSentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ angular/src/app/pages/auth/registration-sent/registration-sent.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './registration-sent.component.html',
+  styleUrls: ['./registration-sent.component.scss'],
+})
+export class RegistrationSentComponent {
+
+}
