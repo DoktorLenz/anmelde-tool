51f3be9cb3ace505c2959c1155fc7c9c7806fbef
Laurenz Stinner <laurenz.stinner2@gmail.com>
2023-04-13 00:02:39 +0200

    unit test localstorageservice and simplify service

--- angular/src/app/core/services/local-storage/local-storage.service.ts
+++ angular/src/app/core/services/local-storage/local-storage.service.ts
@@ -15,12 +15,12 @@ export class LocalStorageService {
     return this.fromLocalStorage(LocalStorageKey.SESSION_AUTHENTICATED) ?? false;
   }
 
-  public setSessionAuthorities(authorities: Authority[] | null) {
+  public setSessionAuthorities(authorities: Authority[]) {
     this.toLocalStorage(authorities, LocalStorageKey.SESSION_AUTHORITIES);
   }
 
-  public getSessionAuthorities(): Authority[] | null {
-    return this.fromLocalStorage(LocalStorageKey.SESSION_AUTHORITIES);
+  public getSessionAuthorities(): Authority[] {
+    return this.fromLocalStorage(LocalStorageKey.SESSION_AUTHORITIES) ?? [];
   }
 
   //#region Storage-Logic
@@ -29,17 +29,13 @@ export class LocalStorageService {
     item: T,
     localStorageKey: LocalStorageKey,
   ): void {
-    try {
-      if (this.isPrimitive(item)) {
-        localStorage.setItem(localStorageKey.toString(), this.b64EncodeUnicode(item.toString()));
-      } else {
-        localStorage.setItem(
-          localStorageKey.toString(),
-          this.b64EncodeUnicode(JSON.stringify(item)),
-        );
-      }
-    } catch (e) {
-      console.error(e);
+    if (this.isPrimitive(item)) {
+      localStorage.setItem(localStorageKey.toString(), item.toString());
+    } else {
+      localStorage.setItem(
+        localStorageKey.toString(),
+        JSON.stringify(item),
+      );
     }
   }
 
@@ -48,12 +44,10 @@ export class LocalStorageService {
     if (!obj) {
       return null;
     }
-    let decoded: string | null = null;
     try {
-      decoded = this.b64DecodeUnicode(obj);
-      return JSON.parse(decoded) as T;
+      return JSON.parse(obj) as T;
     } catch (e) {
-      console.error(`Value of ${localStorageKey} could not be decoded`);
+      console.error(e);
       return null;
     }
   }
@@ -61,35 +55,6 @@ export class LocalStorageService {
   private isPrimitive(value: unknown): value is boolean | number | string {
     const type = typeof value;
     return type === 'string' || type === 'number' || type === 'boolean';
-  }
-
-  private b64EncodeUnicode(a_str: string): string {
-    // eslint-disable-next-line deprecation/deprecation
-    return btoa(
-      encodeURIComponent(a_str).replace(
-        /%([0-9A-F]{2})/g,
-        function (a_match, a_p1) {
-          return String.fromCharCode(parseInt(a_p1, 16));
-        },
-      ),
-    );
-  }
-
-  /**
-   * Decodes a string encoded by {@link b64EncodeUnicode}
-   * @param a_str String to decode
-   * @returns Decoded String
-   * @throws Decoding-Error
-   */
-  private b64DecodeUnicode(a_str: string): string {
-    return decodeURIComponent(
-      Array.prototype.map
-        // eslint-disable-next-line deprecation/deprecation
-        .call(atob(a_str), function (a_c: string) {
-          return '%' + ('00' + a_c.charCodeAt(0).toString(16)).slice(-2);
-        })
-        .join(''),
-    );
   }
   //#endregion
 }
