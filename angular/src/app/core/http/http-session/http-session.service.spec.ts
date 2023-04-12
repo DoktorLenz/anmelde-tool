0226e4119e7cf0e1718a61a56da1ce5d754fddd5
Laurenz Stinner <laurenz.stinner2@gmail.com>
2023-04-12 23:18:32 +0200

    unit test http session service

--- angular/src/app/core/http/http-session/http-session.service.spec.ts
+++ angular/src/app/core/http/http-session/http-session.service.spec.ts
@@ -1,16 +1,35 @@
 import { TestBed } from '@angular/core/testing';
 
 import { HttpSessionService } from './http-session.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('HttpSessionService', () => {
-  let service: HttpSessionService;
+  let httpSessionService: HttpSessionService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(HttpSessionService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [HttpSessionService],
+    });
+    httpSessionService = TestBed.inject(HttpSessionService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  describe('session()', () => {
+    it('should send a GET request to receive JSESSIONID and XSRF Token as Cookie', () => {
+      httpSessionService.session().subscribe((result) => {
+        expect(result).toBeUndefined();
+      });
+
+      const request = httpTestingController.expectOne('/api/v1/session');
+
+      expect(request.request.method).toEqual('GET');
+      request.flush(null);
+    });
   });
 });
