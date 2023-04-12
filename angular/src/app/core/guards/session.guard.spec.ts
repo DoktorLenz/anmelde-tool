a17a0c06ab3bc0fd447f8c08f56e3590dea01b19
Laurenz Stinner <laurenz.stinner2@gmail.com>
2023-04-12 23:28:31 +0200

    use ng mocks for session guard tests

--- angular/src/app/core/guards/session.guard.spec.ts
+++ angular/src/app/core/guards/session.guard.spec.ts
@@ -3,86 +3,95 @@ import { SessionService } from '../services/session/session.service';
 import { BaseRoute } from 'src/app/lib/routes/base-route';
 import { sessionAuthenticatedGuard, sessionNotAuthenticatedGuard } from './session.guard';
 import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
+import { MockProvider, MockService, ngMocks } from 'ng-mocks';
 
 
 describe('SessionGuard', () => {
 
   describe('sessionAuthenticatedGuard', () => {
-    let mockRouter: jasmine.SpyObj<Router>;
-    let mockRoute: jasmine.SpyObj<ActivatedRouteSnapshot>;
-    let mockState: jasmine.SpyObj<RouterStateSnapshot>;
+    let route: ActivatedRouteSnapshot;
+    let state: RouterStateSnapshot;
+    let sessionService: SessionService;
+    let router: Router;
 
     beforeEach(() => {
-      mockRouter = jasmine.createSpyObj<Router>('Router', ['navigate']);
-      mockRoute = jasmine.createSpyObj('ActivatedRouteSnapshot', ['']);
-      mockRoute = jasmine.createSpyObj('RouterStateSnapshot', ['']);
+      route = MockService(ActivatedRouteSnapshot);
+      state = MockService(RouterStateSnapshot);
+
+      TestBed.configureTestingModule({
+        providers: [
+          MockProvider(SessionService),
+          MockProvider(Router),
+        ],
+      });
+
+      sessionService = TestBed.inject(SessionService);
+      router = TestBed.inject(Router);
     });
 
     it('should return true if session is authenticated and not navigate', () => {
-      TestBed.configureTestingModule({
-        providers: [
-          { provide: SessionService, useValue: { isSessionAuthenticated: () => true } },
-          { provide: Router, useValue: mockRouter },
-        ],
+      ngMocks.stub(sessionService, {
+        isSessionAuthenticated: jasmine.createSpy().and.returnValue(true),
       });
 
-      const result = TestBed.runInInjectionContext(() => sessionAuthenticatedGuard(mockRoute, mockState));
+      const result = TestBed.runInInjectionContext(() => sessionAuthenticatedGuard(route, state));
 
       expect(result).toBeTrue();
-      expect(mockRouter.navigate).not.toHaveBeenCalled();
+      expect(router.navigate).not.toHaveBeenCalled();
     });
 
     it('should navigate to auth page if session is not authenticated', () => {
-      TestBed.configureTestingModule({
-        providers: [
-          { provide: SessionService, useValue: { isSessionAuthenticated: () => false } },
-          { provide: Router, useValue: mockRouter },
-        ],
+      ngMocks.stub(sessionService, {
+        isSessionAuthenticated: jasmine.createSpy().and.returnValue(false),
       });
 
-      TestBed.runInInjectionContext(() => sessionAuthenticatedGuard(mockRoute, mockState));
+      TestBed.runInInjectionContext(() => sessionAuthenticatedGuard(route, state));
 
-      expect(mockRouter.navigate).toHaveBeenCalledWith([`/${BaseRoute.AUTH}`]);
+      expect(router.navigate).toHaveBeenCalledWith([`/${BaseRoute.AUTH}`]);
     });
   });
 
 
   describe('sessionNotAuthenticatedGuard', () => {
-    let mockRouter: jasmine.SpyObj<Router>;
-    let mockRoute: jasmine.SpyObj<ActivatedRouteSnapshot>;
-    let mockState: jasmine.SpyObj<RouterStateSnapshot>;
+    let route: ActivatedRouteSnapshot;
+    let state: RouterStateSnapshot;
+    let sessionService: SessionService;
+    let router: Router;
 
     beforeEach(() => {
-      mockRouter = jasmine.createSpyObj<Router>('Router', ['navigate']);
-      mockRoute = jasmine.createSpyObj('ActivatedRouteSnapshot', ['']);
-      mockRoute = jasmine.createSpyObj('RouterStateSnapshot', ['']);
+      route = MockService(ActivatedRouteSnapshot);
+      state = MockService(RouterStateSnapshot);
+
+      TestBed.configureTestingModule({
+        providers: [
+          MockProvider(SessionService),
+          MockProvider(Router),
+        ],
+      });
+
+      sessionService = TestBed.inject(SessionService);
+      router = TestBed.inject(Router);
     });
 
     it('should return true if session is not authenticated and not navigate', () => {
-      TestBed.configureTestingModule({
-        providers: [
-          { provide: SessionService, useValue: { isSessionAuthenticated: () => false } },
-          { provide: Router, useValue: mockRouter },
-        ],
+      ngMocks.stub(sessionService, {
+        isSessionAuthenticated: jasmine.createSpy().and.returnValue(false),
       });
 
-      const result = TestBed.runInInjectionContext(() => sessionNotAuthenticatedGuard(mockRoute, mockState));
+      const result = TestBed.runInInjectionContext(() => sessionNotAuthenticatedGuard(route, state));
 
       expect(result).toBeTrue();
-      expect(mockRouter.navigate).not.toHaveBeenCalled();
+      expect(router.navigate).not.toHaveBeenCalled();
     });
 
     it('should navigate to root page if session is authenticated', () => {
-      TestBed.configureTestingModule({
-        providers: [
-          { provide: SessionService, useValue: { isSessionAuthenticated: () => true } },
-          { provide: Router, useValue: mockRouter },
-        ],
+      ngMocks.stub(sessionService, {
+        isSessionAuthenticated: jasmine.createSpy().and.returnValue(true),
       });
 
-      TestBed.runInInjectionContext(() => sessionNotAuthenticatedGuard(mockRoute, mockState));
+      TestBed.runInInjectionContext(() => sessionNotAuthenticatedGuard(route, state));
 
-      expect(mockRouter.navigate).toHaveBeenCalledWith(['']);
+      expect(router.navigate).toHaveBeenCalledWith(['']);
     });
   });
 });
