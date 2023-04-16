3767f5b369907fba8c1753ae006f86398be4bf19
Laurenz Stinner <laurenz.stinner2@gmail.com>
2023-04-17 00:16:13 +0200

    test error tooltip pipe

--- angular/src/app/lib/pipes/error-tooltip/error-tooltip.pipe.spec.ts
+++ angular/src/app/lib/pipes/error-tooltip/error-tooltip.pipe.spec.ts
@@ -1,9 +1,62 @@
+import { MockBuilder, MockRender, MockService, ngMocks } from 'ng-mocks';
 import { ErrorTooltipPipe } from './error-tooltip.pipe';
+import { AbstractControl } from '@angular/forms';
 
 describe('ErrorTooltipPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ErrorTooltipPipe();
+  ngMocks.faster();
 
-    expect(pipe).toBeTruthy();
+  beforeEach(() => MockBuilder(ErrorTooltipPipe));
+
+  it('should return empty string if control has no errors', () => {
+    const control: AbstractControl = MockService(AbstractControl);
+    const fixture = MockRender('{{ control | errorTooltip }}', {
+      control: control,
+    });
+
+    expect(fixture.nativeElement.innerHTML).toEqual('');
+  });
+
+  it('should return "Erforderlich" if control has required error', () => {
+    const control: AbstractControl = MockService(AbstractControl, {
+      errors: { 'required': true },
+    });
+    const fixture = MockRender('{{ control | errorTooltip }}', {
+      control: control,
+    });
+
+    expect(fixture.nativeElement.innerHTML).toEqual('Erforderlich');
+  });
+
+  it('should return "Gib eine gültige E-Mail Adresse ein" if control has email error', () => {
+    const control: AbstractControl = MockService(AbstractControl, {
+      errors: { 'email': true },
+    });
+    const fixture = MockRender('{{ control | errorTooltip }}', {
+      control: control,
+    });
+
+    expect(fixture.nativeElement.innerHTML).toEqual('Gib eine gültige E-Mail Adresse ein');
+  });
+
+  it('should return "unknown error {error}" if control has unhandled error', () => {
+    const control: AbstractControl = MockService(AbstractControl, {
+      errors: { 'unhandled error': true },
+    });
+    const fixture = MockRender('{{ control | errorTooltip }}', {
+      control: control,
+    });
+
+    expect(fixture.nativeElement.innerHTML).toEqual('unknown error "unhandled error"');
+  });
+
+  it('should join on multiple errors', () => {
+    const control: AbstractControl = MockService(AbstractControl, {
+      errors: { 'required': true, 'email': true  },
+    });
+    const fixture = MockRender('{{ control | errorTooltip }}', {
+      control: control,
+    });
+
+    expect(fixture.nativeElement.innerHTML).toEqual('Erforderlich, Gib eine gültige E-Mail Adresse ein');
   });
 });
--- angular/src/app/lib/pipes/error-tooltip/error-tooltip.pipe.ts
+++ angular/src/app/lib/pipes/error-tooltip/error-tooltip.pipe.ts
@@ -7,12 +7,23 @@ import { AbstractControl } from '@angular/forms';
 export class ErrorTooltipPipe implements PipeTransform {
 
   transform(control: AbstractControl): string {
-    if (control.errors?.['required']) {
-      return 'Erforderlich';
-    } else if (control.errors?.['email']) {
-      return 'Gib eine gültige E-Mail Adresse ein';
-    } else {
-      return JSON.stringify(control.errors);
+    console.warn(control);
+
+    const errorMessages = Object.entries(control.errors ?? [])
+      .map(([property]) => this.getErrorMessage(property));
+
+
+    return errorMessages.join(', ');
+  }
+
+  getErrorMessage(error: string): string {
+    switch (error) {
+      case 'required':
+        return 'Erforderlich';
+      case 'email':
+        return 'Gib eine gültige E-Mail Adresse ein';
+      default:
+        return `unknown error "${error}"`;
     }
   }
 
