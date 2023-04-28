b66243673c0f72acbb299507755cf5e455d8a800
Laurenz Stinner <laurenz.stinner2@gmail.com>
2023-04-28 03:13:10 +0200

    prepare use of testing-library

--- angular/src/test.ts
+++ angular/src/test.ts
@@ -3,8 +3,23 @@ import { BrowserDynamicTestingModule, platformBrowserDynamicTesting }
   from '@angular/platform-browser-dynamic/testing';
 import { ngMocks } from 'ng-mocks';
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+// eslint-disable-next-line import/no-extraneous-dependencies
+import JasmineDOM from '@testing-library/jasmine-dom/dist';
+
 // First, initialize the Angular testing environment.
-getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
+getTestBed().initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting(),
+  { teardown: { destroyAfterEach: false } },
+);
 
 ngMocks.autoSpy('jasmine');
 jasmine.getEnv().allowRespy(true);
+
+beforeEach(() => {
+  jasmine.addMatchers(JasmineDOM);
+});
+
+
