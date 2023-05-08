b9270cbb864594861ac8b21f58c0f57abe135255
Laurenz Stinner <laurenz.stinner2@gmail.com>
2023-05-08 15:09:54 +0200

    -

--- tools/e2e/run.sh
+++ tools/e2e/run.sh
@@ -14,22 +14,20 @@ cd ../tools/e2e
 docker-compose build
 docker-compose up -d
 
-if ! docker ps -f name=backend_e2e | grep -q backend_e2e; then
-    echo "Container backend_e2e is not running"
+container_name="backend_e2e"
+
+if ! docker ps -f name=$container_name | grep -q $container_name; then
+    echo "Container $container_name is not running"
     exit 1
 fi
 
-backend_container_id=$(docker ps -f name=backend_e2e -q)
+while true; do
+    health=$(docker inspect --format='{{json .State.Health.Status}}' $container_name | jq -r '.')
+    if [ "$health" == "healthy" ]; then
+        echo "Container is healthy!"
+        break
+    fi
 
-docker events --filter "container=$container_id" --filter "event=health_status" --format '{{.Status}}' > /tmp/docker-events.log 2>&1 &
-
-# read the event log file and check for container health status
-while read event; do
-  if [ "$event" = "health_status: healthy" ]; then
-    echo "Container backend_e2e is healthy"
-    kill %1 # kill the background process that's reading the event log file
-    break
-  else
-    echo "Container backend_e2e is not healthy"
-  fi
-done < <(tail -n 0 -f /tmp/docker-events.log)
+    echo "Container is still starting up, health status: $health"
+    sleep 5
+done
