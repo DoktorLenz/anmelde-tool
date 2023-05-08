29c80bd4070a81017a1c475c9513ae318b68bda3
Laurenz Stinner <laurenz.stinner2@gmail.com>
2023-05-08 12:46:14 +0200

    Remove special frontend configs

--- docker/angular/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ "$CONFIG" == "e2e" ]; then
-    cp /usr/share/nginx/config/e2e.conf /etc/nginx/conf.d/default.conf
-elif [ "$CONFIG" == "prod" ]; then
-    cp /usr/share/nginx/config/prod.conf /etc/nginx/conf.d/default.conf
-else
-    echo "Unknown configuration value: $CONFIG"
-    exit 1
-fi
