432dab14cd6d325ea58f1cf34695bfa9dbddf74a
Laurenz Stinner <laurenz.stinner2@gmail.com>
2023-06-05 22:31:59 +0200

    Fix ApiEndpoints

--- spring/src/main/java/de/stinner/anmeldetool/application/rest/ApiEndpoints.java
+++ spring/src/main/java/de/stinner/anmeldetool/application/rest/ApiEndpoints.java
@@ -14,8 +14,8 @@ public final class ApiEndpoints {
 
     @UtilityClass
     public static final class Actuator {
-        private static final String BASE = "/actuator";
-        private static final String HEALTH = BASE + "/health";
+        private static final String BASE_ACTUATOR = BASE + "/actuator";
+        private static final String HEALTH = BASE_ACTUATOR + "/health";
         public static final String READINESS = HEALTH + "/readiness";
         public static final String LIVENESS = HEALTH + "/liveness";
     }
