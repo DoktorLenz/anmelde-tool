6d0bbc4f7fad43052523c4850e2d2246e0d51352
Laurenz Stinner <laurenz.stinner2@gmail.com>
2023-06-06 18:22:52 +0200

    Fix use of deprecated features in SecurityConfig

--- spring/src/main/java/de/stinner/anmeldetool/application/auth/WebSecurityConfiguration.java
+++ spring/src/main/java/de/stinner/anmeldetool/application/auth/WebSecurityConfiguration.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
@@ -15,20 +16,34 @@ public class WebSecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.csrf().csrfTokenRepository(new CookieCsrfTokenRepository()).and()
-                .cors().and()
-                .authorizeRequests(req -> req
+        http
+                .csrf().csrfTokenRepository(new CookieCsrfTokenRepository())
+                .and()
+                .cors();
+
+        http
+                .httpBasic().disable()
+                .formLogin().disable()
+                .oauth2ResourceServer(
+                        httpSecurityOAuth2ResourceServerConfigurer -> httpSecurityOAuth2ResourceServerConfigurer
+                                .jwt(jwtConfigurer -> {
+                                }));
+
+        http
+                .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
+
+        http
+                .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(
                                 ActuatorEndpoints.READINESS,
                                 ActuatorEndpoints.LIVENESS
-                        ).permitAll()
+                        )
+                        .permitAll()
                         .anyRequest()
                         .authenticated()
-                )
-                .httpBasic().disable()
-                .formLogin().disable()
-                .oauth2ResourceServer(config -> config.jwt(jwtConfigurer -> {
-                }));
+                );
 
         return http.build();
     }
