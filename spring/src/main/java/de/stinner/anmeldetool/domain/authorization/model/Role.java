34ab80d005558a5f22fe37b909f608887eab90e6
Laurenz Stinner <laurenz.stinner2@gmail.com>
2023-06-10 21:24:25 +0200

    Switched Role Enum to static string values

--- spring/src/main/java/de/stinner/anmeldetool/domain/authorization/model/Role.java
+++
@@ -1,7 +0,0 @@
-package de.stinner.anmeldetool.domain.authorization.model;
-
-public enum Role {
-    // Verified a basic role. Without it a user is not able to do anything.
-    VERIFIED,
-    ADMIN
-}
---
+++ spring/src/main/java/de/stinner/anmeldetool/domain/authorization/userroles/model/Role.java
@@ -0,0 +1,9 @@
+package de.stinner.anmeldetool.domain.authorization.userroles.model;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public final class Role {
+    public static final String VERIFIED = "VERIFIED";
+    public static final String ADMIN = "ADMIN";
+}
