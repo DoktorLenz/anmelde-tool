24e8a354c9fd81adea9cce002413bd1393ecce45
Laurenz Stinner <laurenz.stinner2@gmail.com>
2023-06-15 04:38:27 +0200

    cleanup app component

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -4,10 +4,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ButtonModule } from 'primeng/button';
-import { ToastModule } from 'primeng/toast';
 import { HttpClientModule, HttpClientXsrfModule } from '@angular/common/http';
 import { AuthConfigModule } from './auth/auth-config.module';
+import { AuthDirectivesModule } from './auth/directives/auth-directives.module';
+import { NavigationModule } from './navigation/navigation.module';
 
 @NgModule({
   declarations: [
@@ -22,9 +22,9 @@ import { AuthConfigModule } from './auth/auth-config.module';
       cookieName: 'XSRF-TOKEN',
       headerName: 'X-XSRF-TOKEN',
     }),
-    ToastModule,
-    ButtonModule,
     AuthConfigModule,
+    AuthDirectivesModule,
+    NavigationModule,
   ],
   providers: [
   ],
