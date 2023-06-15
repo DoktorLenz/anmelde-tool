e73088dacb8a682aa5400557167885e27ac80be6
Laurenz Stinner <laurenz.stinner2@gmail.com>
2023-06-16 00:26:39 +0200

    store user information in local storage

--- angular/src/app/auth/services/userdata/user-data.service.ts
+++ angular/src/app/auth/services/userdata/user-data.service.ts
@@ -1,31 +1,33 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { EventTypes, OidcClientNotification, PublicEventsService } from 'angular-auth-oidc-client';
-import { Observable, ReplaySubject, filter } from 'rxjs';
+import { BehaviorSubject, Observable, filter } from 'rxjs';
 import { OidcUserData } from '../../models/oidc-user-data';
 import { UserData } from '../../models/user-data';
 import { Role } from '../../models/role.enum';
+import { LocalStorageService } from 'src/app/storage/local-storage/local-storage.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserDataService {
 
-  private _userDataSubject: ReplaySubject<UserData> = new ReplaySubject(1);
+  private _userDataSubject: BehaviorSubject<UserData> =
+    new BehaviorSubject(this.localStorageService.userData);
 
   public userData$: Observable<UserData> = this._userDataSubject.asObservable();
+
+  private updateUserData(userData: UserData): void {
+    this.localStorageService.userData = userData;
+    this._userDataSubject.next(userData);
+  }
 
   constructor(
     private readonly http: HttpClient,
     private readonly eventService: PublicEventsService,
+    private readonly localStorageService: LocalStorageService,
   ) {
-    this.eventService
-      .registerForEvents()
-      .pipe(
-        // filter((notification) => notification.type === EventTypes.NewAuthenticationResult),
-      )
-      .subscribe((x) => console.warn(x));
-
+    // There will only be an event on login
     this.eventService
       .registerForEvents()
       .pipe(
@@ -35,10 +37,11 @@ export class UserDataService {
         next: (notification: OidcClientNotification<OidcUserData>) => {
           this.http.get<Role[]>('/api/v1/auth/user-roles').subscribe({
             next: (roles: Role[]) => {
-              this._userDataSubject.next({ ...notification.value, authorities: roles });
+              this.updateUserData({ ...notification.value, authorities: roles });
             },
           });
         },
       });
+
   }
 }
