1320113ed7c0fcc39b6aa238d30ae195cdc15c13
Laurenz Stinner <laurenz.stinner2@gmail.com>
2023-06-16 01:20:11 +0200

    close mobile navbar on nav

--- angular/src/app/navigation/sidebar/sidebar.component.ts
+++ angular/src/app/navigation/sidebar/sidebar.component.ts
@@ -1,12 +1,15 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { NavigationService } from '../navigation.service';
-import { Observable } from 'rxjs';
+import { Observable, filter, tap } from 'rxjs';
 import { Breakpoint } from 'src/app/layout/directives/breakpoint/breakpoint.enum';
 import { NumberComparator } from 'src/app/layout/directives/breakpoint/comparator';
 import { UserDataService } from 'src/app/auth/services/userdata/user-data.service';
 import { UserData } from 'src/app/auth/models/user-data';
 import { Role } from 'src/app/auth/models/role.enum';
+import { BaseRoute } from 'src/app/lib/routes/base-route.enum';
+import { UserManagementRoute } from 'src/app/lib/routes/user-management-route.enum';
+import { EventType, Router } from '@angular/router';
 
 @Component({
   selector: 'at-sidebar',
@@ -20,6 +23,7 @@ export class SidebarComponent {
     {
       label: 'Home',
       icon: 'pi pi-home',
+      routerLink: `${BaseRoute.HOME}`,
     },
   ];
 
@@ -30,9 +34,11 @@ export class SidebarComponent {
       items: [
         {
           label: 'Übersicht',
+          routerLink: `${BaseRoute.USER_MANAGEMENT}/${UserManagementRoute.OVERVIEW}`,
         },
         {
           label: 'NaMi-Mitglieder',
+          routerLink: `${BaseRoute.USER_MANAGEMENT}/${UserManagementRoute.NAMI_MEMBERS}`,
         },
       ],
     },
@@ -55,8 +61,8 @@ export class SidebarComponent {
   constructor(
     private readonly navigationService: NavigationService,
     private readonly userDataService: UserDataService,
+    private readonly router: Router,
   ) {
-
     this.userDataService.userData$.subscribe({
       next: (userData: UserData) => {
         this.menuItems = [...this.userMenuItems];
@@ -65,5 +71,16 @@ export class SidebarComponent {
         }
       },
     });
+
+    this.router.events
+      .pipe(
+        filter(
+          (routerEvent) =>
+            routerEvent.type === EventType.NavigationStart ||
+            routerEvent.type === EventType.NavigationSkipped,
+        ),
+      ).subscribe(() => {
+        this.sidebarVisibleChange(false);
+      });
   }
 }
