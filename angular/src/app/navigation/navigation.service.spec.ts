23bab20793b501d87a5878e66ed3e492cb10a7f6
Laurenz Stinner <laurenz.stinner2@gmail.com>
2023-06-27 23:31:52 +0200

    UnitTest NavigationService

--- angular/src/app/navigation/navigation.service.spec.ts
+++ angular/src/app/navigation/navigation.service.spec.ts
@@ -1,16 +1,34 @@
-import { TestBed } from '@angular/core/testing';
-
+import { MockBuilder, ngMocks } from 'ng-mocks';
 import { NavigationService } from './navigation.service';
+import { fakeAsync, flushMicrotasks } from '@angular/core/testing';
 
-describe('NavigationService', () => {
-  let service: NavigationService;
+describe('NavigationService sidebarVisible$', () => {
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(NavigationService);
-  });
+  ngMocks.faster();
+  beforeEach(() => MockBuilder(NavigationService));
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+  it('should hide sidebar initially', fakeAsync(() => {
+    const navigationService = ngMocks.findInstance(NavigationService);
+    let sidebarVisible;
+    navigationService.sidebarVisible$.subscribe((visible: boolean) => {
+      sidebarVisible = visible;
+    });
+
+    flushMicrotasks();
+
+    expect(sidebarVisible).toBeFalse();
+  }));
+
+  it('should publish visibility change of the sidebar', fakeAsync(() => {
+    const navigationService = ngMocks.findInstance(NavigationService);
+    let sidebarVisible;
+    navigationService.sidebarVisible$.subscribe((visible: boolean) => {
+      sidebarVisible = visible;
+    });
+
+    navigationService.sidebarVisible(true);
+    flushMicrotasks();
+
+    expect(sidebarVisible).toBeTrue();
+  }));
 });
