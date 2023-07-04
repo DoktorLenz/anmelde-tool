e0188bdfca6a50d89bba86fcad77ab26d17ccf11
Laurenz Stinner <laurenz.stinner2@gmail.com>
2023-07-04 14:03:04 +0200

    Test for Rank Enum

---
+++ spring/src/test/java/de/stinner/anmeldetool/domain/shared/model/RankTest.java
@@ -0,0 +1,17 @@
+package de.stinner.anmeldetool.domain.shared.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RankTest {
+
+    @Test
+    void test_string_to_Rank() {
+        assertThat(Rank.WOELFLING.getRank()).isEqualTo("Wölfling");
+        assertThat(Rank.JUNGPFADFINDER.getRank()).isEqualTo("Jungpfadfinder");
+        assertThat(Rank.PFADFINDER.getRank()).isEqualTo("Pfadfinder");
+        assertThat(Rank.ROVER.getRank()).isEqualTo("Rover");
+        assertThat(Rank.UNDEFINED.getRank()).isEmpty();
+    }
+}
