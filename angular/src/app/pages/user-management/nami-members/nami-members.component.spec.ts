48c96f3a7465f45f2ac24195b1bcb6a06cd88b73
Laurenz Stinner <laurenz.stinner2@gmail.com>
2023-07-30 21:33:20 +0200

    Fix tests

--- angular/src/app/pages/user-management/nami-members/nami-members.component.spec.ts
+++ angular/src/app/pages/user-management/nami-members/nami-members.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NamiMembersComponent } from './nami-members.component';
+import { MockProvider } from 'ng-mocks';
+import { NamiMembersService } from 'src/app/user-management/nami-members/services/nami-members.service';
+import { NEVER } from 'rxjs';
 
 describe('NamiMembersComponent', () => {
   let component: NamiMembersComponent;
@@ -9,6 +12,9 @@ describe('NamiMembersComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [NamiMembersComponent],
+      providers: [
+        MockProvider(NamiMembersService, { getNamiMembers: jasmine.createSpy().and.returnValue(NEVER) }),
+      ],
     });
     fixture = TestBed.createComponent(NamiMembersComponent);
     component = fixture.componentInstance;
