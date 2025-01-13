e4d8a46bca5a472054b92b836626795b9c7bcff6
DoktorLenz <laurenz.stinner2@gmail.com>
2025-01-13 22:47:29 +0100

    Fix primeng

--- angular/src/main.ts
+++ angular/src/main.ts
@@ -1,34 +1,52 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-
-import { provideHttpClient, withInterceptorsFromDi, withXsrfConfiguration } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+  withXsrfConfiguration,
+} from '@angular/common/http';
+import { importProvidersFrom, isDevMode } from '@angular/core';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import Aura from '@primeng/themes/aura';
+import { providePrimeNG } from 'primeng/config';
 import { AppRoutingModule } from './app/app-routing.module';
+import { AppComponent } from './app/app.component';
 import { AuthConfigModule } from './app/auth/auth-config.module';
 import { NavigationModule } from './app/navigation/navigation.module';
-import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './app/reducers';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { isDevMode, importProvidersFrom } from '@angular/core';
-import { AppComponent } from './app/app.component';
+import { metaReducers, reducers } from './app/reducers';
 
 bootstrapApplication(AppComponent, {
-    providers: [
-        importProvidersFrom(BrowserModule, AppRoutingModule, AuthConfigModule, NavigationModule, StoreModule.forRoot(reducers, { metaReducers }), EffectsModule.forRoot(), StoreDevtoolsModule.instrument({
-            maxAge: 25,
-            logOnly: !isDevMode(),
-            autoPause: true,
-            trace: true,
-            traceLimit: 75,
-            connectInZone: true,
-        })),
-        provideHttpClient(withInterceptorsFromDi(), withXsrfConfiguration({
-            cookieName: 'XSRF-TOKEN',
-            headerName: 'X-XSRF-TOKEN',
-        })),
-        provideAnimations(),
-    ]
-})
-  .catch(err => console.error(err));
+  providers: [
+    importProvidersFrom(
+      BrowserModule,
+      AppRoutingModule,
+      AuthConfigModule,
+      NavigationModule,
+      StoreModule.forRoot(reducers, { metaReducers }),
+      EffectsModule.forRoot(),
+      StoreDevtoolsModule.instrument({
+        maxAge: 25,
+        logOnly: !isDevMode(),
+        autoPause: true,
+        trace: true,
+        traceLimit: 75,
+        connectInZone: true,
+      })
+    ),
+    provideHttpClient(
+      withInterceptorsFromDi(),
+      withXsrfConfiguration({
+        cookieName: 'XSRF-TOKEN',
+        headerName: 'X-XSRF-TOKEN',
+      })
+    ),
+    provideAnimations(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    }),
+  ],
+}).catch(err => console.error(err));
